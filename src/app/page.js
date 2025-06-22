526d0f67545c4db46def60018bf04b77672d0fe2
Samir <samiirtamang@gmail.com>
2025-06-22 16:18:39 +0545

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,64 @@
 import Image from "next/image";
+import { MdCheckCircle } from "react-icons/md";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <picture className={styles.imageContainer}>
+          <source
+            srcSet="/assets/images/illustration-sign-up-desktop.svg"
+            media="(min-width: 1440px)"
+          />
+          <source
+            srcSet="/assets/images/illustration-sign-up-tablet.svg"
+            media="(min-width: 768px)"
+          />
+          <Image
+            className={styles.image}
+            src="/assets/images/illustration-sign-up-mobile.svg"
+            alt="Illustration artwork"
+            width={375}
+            height={284}
+            priority
+          />
+        </picture>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Stay Updated!</h1>
+          <p className={styles.description}>
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+          <ul className={styles.list}>
+            <li>
+              <MdCheckCircle className={styles.icon} />
+              Product discovery and building what matters
+            </li>
+            <li>
+              <MdCheckCircle className={styles.icon} />
+              Measuring to ensure updates are a success
+            </li>
+            <li>
+              <MdCheckCircle className={styles.icon} />
+              And much more!
+            </li>
+          </ul>
+          <form className={styles.form}>
+            <label className={styles.label}>
+              <span className={styles.labelText}>Email address</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email@company.com"
+              className={styles.input}
+              required
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <button type="submit" className={styles.button}>
+              Subscribe to monthly newsletter
+            </button>
+          </form>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
