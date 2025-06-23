2996bb621d2b709f5e21261034f7c5dc94297dd8
samirtamang33 <152047704+samirtamang33@users.noreply.github.com>
2025-06-23 08:40:19 +0545

    complete Email Error, Submit Success

--- src/app/page.js
+++ src/app/page.js
@@ -1,63 +1,144 @@
+"use client";
+
 import Image from "next/image";
 import { MdCheckCircle } from "react-icons/md";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      setSuccess(false);
+    } else {
+      setError("");
+      setSuccess(true);
+    }
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <picture className={styles.imageContainer}>
-          <source
-            srcSet="/assets/images/illustration-sign-up-desktop.svg"
-            media="(min-width: 1440px)"
-          />
-          <source
-            srcSet="/assets/images/illustration-sign-up-tablet.svg"
-            media="(min-width: 768px)"
-          />
-          <Image
-            className={styles.image}
-            src="/assets/images/illustration-sign-up-mobile.svg"
-            alt="Illustration artwork"
-            width={375}
-            height={284}
-            priority
-          />
-        </picture>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Stay Updated!</h1>
-          <p className={styles.description}>
-            Join 60,000+ product managers receiving monthly updates on:
-          </p>
-          <ul className={styles.list}>
-            <li>
-              <MdCheckCircle className={styles.icon} />
-              Product discovery and building what matters
-            </li>
-            <li>
-              <MdCheckCircle className={styles.icon} />
-              Measuring to ensure updates are a success
-            </li>
-            <li>
-              <MdCheckCircle className={styles.icon} />
-              And much more!
-            </li>
-          </ul>
-          <form className={styles.form}>
-            <label className={styles.label}>
-              <span className={styles.labelText}>Email address</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email@company.com"
-              className={styles.input}
-              required
+        {!success && (
+          <picture className={styles.imageContainer}>
+            <source
+              srcSet="/assets/images/illustration-sign-up-desktop.svg"
+              media="(min-width: 1440px)"
             />
+            <source
+              srcSet="/assets/images/illustration-sign-up-tablet.svg"
+              media="(min-width: 768px)"
+            />
+            <Image
+              className={styles.image}
+              src="/assets/images/illustration-sign-up-mobile.svg"
+              alt="Illustration artwork"
+              width={375}
+              height={284}
+              priority
+            />
+          </picture>
+        )}
+        <div className={styles.content}>
+          {/* If Submit Successful,hide wrap element and show success message */}
+          {!success ? (
+            <>
+              <h1 className={styles.title}>Stay Updated!</h1>
+              <p className={styles.description}>
+                Join 60,000+ product managers receiving monthly updates on:
+              </p>
+              <ul className={styles.list}>
+                <li>
+                  <MdCheckCircle className={styles.icon} />
+                  Product discovery and building what matters
+                </li>
+                <li>
+                  <MdCheckCircle className={styles.icon} />
+                  Measuring to ensure updates are a success
+                </li>
+                <li>
+                  <MdCheckCircle className={styles.icon} />
+                  And much more!
+                </li>
+              </ul>
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <label className={styles.label}>
+                  <span className={styles.labelText}>Email address</span>
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="email@company.com"
+                  className={`${styles.input} ${
+                    error ? styles.inputError : ""
+                  }`}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                {/* Email Error */}
+                {error && (
+                  <span
+                    style={{
+                      color: "var(--color-primary-red)",
+                      fontSize: "0.9rem",
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    {error}
+                  </span>
+                )}
 
-            <button type="submit" className={styles.button}>
-              Subscribe to monthly newsletter
-            </button>
-          </form>
+                <button type="submit" className={styles.button}>
+                  Subscribe to monthly newsletter
+                </button>
+              </form>
+              {/* If Submit Successful, show success message */}
+            </>
+          ) : (
+            <div className={styles.successMessage}>
+              <MdCheckCircle
+                style={{
+                  color: "var(--color-primary-red)",
+                  fontSize: "4rem",
+                  marginBottom: "2rem",
+                }}
+              />
+              <h1
+                style={{
+                  color: "var(--color-blue-700)",
+                  fontSize: "2.5rem",
+                  marginBottom: "2rem",
+                }}
+              >
+                Thanks for subscribing!
+              </h1>
+              <p
+                style={{
+                  fontSize: "1rem",
+                  color: "var(--color-blue-800)",
+                  fontWeight: "400",
+                  lineHeight: "1.5",
+                  marginBottom: "19.93rem",
+                }}
+              >
+                A confirmation email has been sent to <strong>{email}</strong>
+                .com. Please open it and click the button inside to confirm your
+                subscription
+              </p>
+              <button type="submit" className={styles.button}>
+                Dismiss message
+              </button>
+            </div>
+          )}
         </div>
       </main>
     </div>
